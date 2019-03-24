183f1b1eb9c6904177c28c3b1233dea54aee48e2
Marcin Graja <36998161+trahatar@users.noreply.github.com>
2019-03-24 21:39:35 +0100

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+public class Calculator {
+    private static void createAndShowGUI() {
+        //Make sure we have nice window decorations.
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String []args){
+        createAndShowGUI();
+    }
+}
