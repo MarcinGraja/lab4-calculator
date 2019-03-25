95aa331d838b98f5ee13638870fabce05a91f0de
Marcin Graja <36998161+trahatar@users.noreply.github.com>
2019-03-25 01:26:02 +0100

    Created window, added buttons to it and added managing of calculations and their history

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,22 +1,13 @@
 import javax.swing.*;
+import java.text.MessageFormat;
+import java.util.Date;
+
 public class Calculator {
-    private static void createAndShowGUI() {
-        //Make sure we have nice window decorations.
-        JFrame.setDefaultLookAndFeelDecorated(true);
 
-        //Create and set up the window.
-        JFrame frame = new JFrame("HelloWorldSwing");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //Add the ubiquitous "Hello World" label.
-        JLabel label = new JLabel("Hello World");
-        frame.getContentPane().add(label);
-
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
-    public static void main(String []args){
-        createAndShowGUI();
+    public static void main(String[] args) {
+        JFrame calcForm = new CalcForm();
+        calcForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        calcForm.pack();
+        calcForm.setVisible(true);
     }
 }
